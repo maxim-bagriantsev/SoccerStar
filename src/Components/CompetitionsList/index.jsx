f22b3786c59b97bf744bd1942604de167b5c7273
Maxim Bagriantsev <maxim163rus@gmail.com>
2021-07-06 19:21:53 +0400

    add table antd CompetitionsList

--- src/Components/CompetitionsList/index.jsx
+++ src/Components/CompetitionsList/index.jsx
@@ -1,7 +1,13 @@
 import React, {useEffect, useState} from 'react';
+
 import 'antd/dist/antd.css';
-import s from './api.module.css'
+import '../CompetitionsList/index.css';
+import {Table, Input, Button, Space } from "antd";
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
+
 import {getCompetitionsList} from "../../api/competitions";
+
 
 export const CompetitionsList = () => {
     const [competitionsData, setCompetitionsData] = useState(null); //Тут будут данные с сервера
@@ -9,7 +15,7 @@ export const CompetitionsList = () => {
 
 
     useEffect(() => {
-        getCompetitionsList ().then((data)=>{
+        getCompetitionsList().then((data) => {
             setCompetitionsData(data)
         })
             .catch((err) => {
@@ -23,24 +29,65 @@ export const CompetitionsList = () => {
         return <p>Loaded...</p>
     }
 
+    const culumns = [ // Название колонок в таблице
+        {
+            title: "Название соревнования",
+            dataIndex: "nameCompetition",
+            key: "nameCompetition",
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: "Местро проведения",
+            dataIndex: "area",
+            key: "area",
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: "Код страны",
+            dataIndex: "countryCode",
+            key: "countryCode",
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: "Дата начала соревнований",
+            dataIndex: "startDate",
+            key: "startDate",
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: "Дата окончания соревнований",
+            dataIndex: "andDate",
+            key: "andDate",
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: "Код лиги",
+            dataIndex: "leagueCod",
+            key: "leagueCod",
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: "Фдаг страны",
+            dataIndex: "countryFlag",
+            key: "countryFlag",
+            render: (text) => <a>{text}</a>
+        },
+
+    ];
+
     return (
-        <table className={s.table}>
-            <tr>
-                <th>Название лиги</th>
-                <th>Дата начала сезона</th>
-                <th>Место проведения</th>
-            </tr>
-            {competitionsData.competitions.map((item) => (
-                <tr key={item.id}>
-                    <td>{item.name}</td>
-                    <td>{item.area.id}</td>
-                    <td>{item.area.name}</td>
-                </tr>
-            ))
+        <Table columns={culumns} dataSource={competitionsData.competitions.map((item) => {
+            return {
+                key: item.id,
+                nameCompetition: item.name,
+                area: item.area.name,
+                countryCode: item.area.countryCode,
+                leagueCod: item.code,
+                countryFlag: '#',
+                startDate: "#startDate",
+                andDate: "#andDate",
             }
-        </table>
+        })}/>
     )
 }
-
-
 
